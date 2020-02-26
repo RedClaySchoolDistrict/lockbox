ef022bbc5427db064cf6aae06983a54ead019976
Thomas Mack <thomasmack@gmail.com>
2020-02-26 16:41:21 -0500

    PageUpdates

--- js/lockbox.js
+++ js/lockbox.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
     $("body").css("background-image", "url('images/" + Math.floor((Math.random() * 8) + 1) + ".jpg')");
 
     $("button").click(function () {
-        location.replace("https://clever.com/in/red-clay");
+        let url = $(this).attr("data-url");
+        location.replace(url);
     });
 
-    if (navigator.languages.includes("es")) {
+    /*if (navigator.languages.includes("es")) {
         $("#header").text("ACCESO DENEGADO"); // Access Denied
         $("#message1").text("El dispositivo que está utilizando se ha colocado en la caja de bloqueo. Sólo puede acceder a sitios web aprobados."); // Message above button
         $("button").html('<i class="fas fa-th mr-2"></i> De vuelta a Clever'); // Button text 
         $("#message2").text("Si sientes que esto es incorrecto por favor contacta a la bibliotecaria de tu escuela."); // Message below button
-    }
+    }*/
 });
